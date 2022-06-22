3c20ddcdfd257921f7be0e7784a8b1ddaa2d5642
root <root@debian.longnm>
2022-06-22 12:15:51 -0400

    rm LDFLAGS -lm

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ ROOT_DIR=$(shell pwd)
 OBJ_DIR=build
 HEADER_DIR=ak
 EXAMPLE_DIR=example
-LDFLAGS += -lm -lpthread -lrt
+LDFLAGS += -lpthread -lrt
 CFLAGS	+= -I./ -std=gnu99 -fPIC -finline-functions -pipe -fPIC -fwrapv -Wl,--gc-sections -Wl,--print-gc-sections -fdata-sections -ffunction-sections -O2
 LDLIBS += -shared -Wl,-soname,$(NAME_MODULE)
 VERSION=1
